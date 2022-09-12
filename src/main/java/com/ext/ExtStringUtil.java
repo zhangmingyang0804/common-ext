15c648bdd4cedd3cb5ceca72e4abcfbab03b3dce
zhangmingyang <zhangmingyang@foonsu.com>
2022-09-12 16:35:36 +0800

    初始化

--- src/main/java/com/ext/ExtStringUtil.java
+++ src/main/java/com/ext/ExtStringUtil.java
@@ -1,4 +1,13 @@
 package com.ext;
 
 public class ExtStringUtil {
+    /**
+     * 判断字符串为空
+     *
+     * @param val
+     * @return
+     */
+    public static boolean isEmpty(String val) {
+        return null == val || val.trim().length() == 0;
+    }
 }
